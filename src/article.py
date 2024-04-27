1656eed2a3058e698756c607d00d4fcb5fa9e4d3
helonely <idravil1211@gmail.com>
2024-04-27 12:18:35 +0500

    add method

--- src/article.py
+++ src/article.py
@@ -2,8 +2,50 @@ class Article:
     """
     Класс для хранения статьи
     """
-    def __init__(self):
+    article_id: int
+    title: str
+    content: str
+
+    articles = dict()  # атрибут на уровне класса для хранения всех статей
+
+    def __init__(self, title: str, content: str):
         """
         Конструктор для статьи
         """
-        pass
+        self.article_id = self.get_new_id()
+        self.title = title
+        self.content = content
+
+    def get_new_id(self) -> int:
+        """
+        Метод добавляет ID следующей статьи
+        """
+        if len(self.articles) > 0:
+            return max(self.articles.keys()) + 1
+        return 1
+
+    @classmethod
+    def insert(cls, title: str, content: str):
+        """
+        Метод для создания и добавления статьи
+        :param title:
+        :param content:
+        :return:
+        """
+        new_article = cls(title, content)
+        cls.articles[new_article.article_id] = new_article
+        return new_article
+
+    @classmethod
+    def search(cls, article_id):
+        """
+        Метод для поиска статьи по ID
+        """
+        return cls.articles[article_id]
+
+if __name__ == '__main__':
+    new_articles_1 = Article.insert('test1', 'test1')
+    print(new_articles_1.article_id)
+
+    new_articles_2 = Article.insert('test2', 'test2')
+    print(new_articles_2.article_id)
